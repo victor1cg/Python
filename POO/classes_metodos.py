2a4f930a87eb86b73f7ea42c4eeb377c748d97d5
Victor Gonçalves <victor1cg@hotmail.com>
2022-06-13 12:55:48 -0300

    Add details

--- POO/classes_metodos.py
+++ POO/classes_metodos.py
@@ -1,5 +1,10 @@
 #classes e metodos
+# Estudo do Dev Aprender
 
+
+# __init__ chamado de consultor, serve para os parametros iniciais;
+# self - server para acessar os metodos e propriedades da instancia. Podem ser acessadas de outro lugar.
+#  
 class computador:               #é necessario passar os parametros, se não fica chumbado p/ todos
     def __init__(self, marca,memoria_ram,placa_video):                 
         self.marca = marca
@@ -8,6 +13,9 @@ class computador:               #é necessario passar os parametros, se não fic
         
     def info(self):
         print(self.marca, self.memoria_ram, self.placa_video)
+
+    def Ligar(self):
+        print("Estou ligando")
         
 pc1 = computador('Asus','8gb','Nvidia')
 pc2 = computador('Dell','16gb','Geforce')
@@ -15,7 +23,7 @@ pc3 = computador('HP','4gb','Geforce')
 pc1.info
 #--------- Aula Inicial Python Orientado ao Objeto Boson
 
-class Cubo 
+class Cubo: 
         '''Criar uma classe que calcula o valor do cubo'''
         def __init__(self,valor):
             self.x = valor                    # com o self a variável x é global / Metodo construtor
